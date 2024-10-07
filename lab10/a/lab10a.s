75070a2f141d01d94a34686744740b2fd9a8e740
Taina Bastos de Almeida <ra253350@nicaragua.lab.ic.unicamp.br>
2024-10-07 12:21:57 -0300

    puts

--- lab10/a/lab10a.s
+++ lab10/a/lab10a.s
@@ -1,3 +1,5 @@
+.bss
+string_puts: .skip 
 .text
 
 .globl linked_list_search
@@ -6,6 +8,7 @@
 .globl atoi
 .globl itoa
 .globl exit
+
 
 linked_list_search:
 
@@ -16,12 +19,15 @@ puts:
         sw ra, 0(sp)
         lb a1, 0(a2)        # carrega o primeiro digito na string enderecada
         beqz a1, fim_linha
+        sb a1, 0(a0)
         addi a2, a2, 1
         j 1b
-
 fim_linha:
-    li a0
+    li a1, 0        # '\0'
+    sb a1, 0(a2)
+    mv a1, a2
+    ret
 
 write:
     li a0, 1
-    li
+    la a1
