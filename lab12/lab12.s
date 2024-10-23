f749f5bdd01d36216b36e448188ebefdd41b0b1d
Taina Bastos de Almeida <ra253350@elsalvador.lab.ic.unicamp.br>
2024-10-23 16:50:22 -0300

    na verdade nao ta OK :P

--- lab12/lab12.s
+++ lab12/lab12.s
@@ -5,7 +5,12 @@ _start:
     li a7, 93
     ecall
 
+.bss
+string_input: .skip 35
+
 .text
+
+
 .set trigger_write,0xFFFF0100
 .set write_byte, 0xFFFF0101
 .set trigger_read, 0xFFFF0102
@@ -35,18 +40,27 @@ main:
 get_operacao:
     addi sp, sp, -16
     sw ra, (sp)
+
     li t1, 1
-    sb t1, (t0)            # carrega status 1 em t0
+    li t0, trigger_read         # leitura
+    sb t1, (t0)                 # carrega status 1 em t0
+    
     1:
-        li t0, trigger_read    # leitura
-        li t1, read_byte
+        # li t0, trigger_read    
+        # li t1, 1
+        # sb t1, (t0)            
+        
+        # li t1, read_byte
         lb t2, (t0)
         bnez t2, 1b            # continua lendo enquanto nao tiver finalizado
 
-    li t1, read_byte
-    lb a0, (t3)            # le byte com numero da operacao
+    
+    li t0, read_byte
+    lb a0, (t0)            # le byte com numero da operacao
 
-    li t1, read_byte       # le \n
+    li t2, 1
+    sb t2, (t0)
+    li t0, read_byte       # le \n
 
     lw ra, (sp)
     addi sp, sp, 16
@@ -56,23 +70,38 @@ get_operacao:
 puts:
     addi sp, sp, -16
     sw ra, 0(sp)
-    mv t0, a0
-    li t2, 0
-    li a2, 0
+    la s1, string_input
+    li t2, 1
+    sb t2, (t0)
+    sb t2, (t1)
+    li a1, 0
+    1:
+        
+        li t1, trigger_read
+        li t2, read_byte
+        li t3, write_byte
+        addi a1, a1, 1
+        sb t2, (s1)
+        addi s1, s1, 1
+        lb t2, (t0)
+        bnez t2, 1b
     
-    loop_puts:
-        lb t1, 0(t0)        # carrega o primeiro digito na string enderecada
-        addi t0, t0, 1
-        addi t2, t2, 1
-        beqz t1, fim_puts
-        j loop_puts
 fim_puts:
     li t1, 10
     sb t1, 0(t0)
-    addi t2, t2, 1
-    mv a2, t2
-    mv a1, a0
-    
+    addi a1, a1, 1
+
+    write:
+        loop_puts:
+        li t0, trigger_write
+        sub s1, s1, a1
+        lbu t1, (s1)
+        sb t1, (t3)
+        li t3, write_byte
+        addi s1, s1, 1
+        lb t4, (t0)
+        bnez t4, write
+
     lw ra, 0(sp)
     addi sp, sp, 16
     ret
